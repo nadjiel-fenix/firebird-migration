d0857709fce2301407ff4bcbdab4476fef4f7b5a
nadjiel-fenix <daniel@fenixtecnologia.info>
2025-12-29 17:14:08 -0300

    :sparkles: feat: criado o scritp reparar-banco.ps1

---
+++ firebird-v3/reparar-banco.ps1
@@ -0,0 +1,31 @@
+# Executa o script de reparação de banco de dados,
+# que corrige os erros identificados previamente nos bancos,
+# como ambiguidade ou uso errado de SUSPEND.
+
+param (
+    [Parameter(Position = 0, Mandatory)]
+    [Alias('sistema')]
+    [string]$system,
+
+    [Parameter(Position = 1, Mandatory)]
+    [Alias('banco')]
+    [string]$database
+)
+
+# Lê as configurações do banco
+$config = Get-Content ".\config.json" | ConvertFrom-Json
+
+# Configura isql para apontar para o isql do firebird da versão atual
+Set-Alias isql "$($config.firebird)\isql.exe"
+
+# Guarda o diretório do sistema e do banco
+$system_dir = ".\${system}"
+$db_dir = "${system_dir}\bancos\${database}"
+
+# Concatena os caminhos do script de reparação, do banco e do arquivo de log
+$repair_script = "${system_dir}\reparador.sql"
+$db_path = "${db_dir}\in.fdb"
+$log_path = "${db_dir}\reparo.log"
+
+# Executa a reparação no banco de dados e armazena os logs
+isql -user SYSDBA -password masterkey "$db_path" -i "$repair_script" -o "$log_path" -m
