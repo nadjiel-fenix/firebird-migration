c22e7bb01370c259f30b9582e65dab86c06aaab3
nadjiel-fenix <daniel@fenixtecnologia.info>
2026-01-02 16:57:15 -0300

    :recycle: refac: movido reparar-banco.ps1 para raíz por conveniência

--- reparar-banco.ps1
+++ reparar-banco.ps1
@@ -4,26 +4,32 @@
 
 param (
     [Parameter(Position = 0, Mandatory)]
+    [ValidateSet(3, 5)]
+    [Alias('versao')]
+    [int]$version,
+
+    [Parameter(Position = 1, Mandatory)]
     [Alias('sistema')]
     [string]$system,
 
-    [Parameter(Position = 1, Mandatory)]
+    [Parameter(Position = 2, Mandatory)]
     [Alias('banco')]
     [string]$database
 )
 
 # Lê as configurações do banco
-$config = Get-Content ".\config.json" | ConvertFrom-Json
+$config = Get-Content ".\firebird-v${version}\config.json" | ConvertFrom-Json
 
 # Configura isql para apontar para o isql do firebird da versão atual
 Set-Alias isql "$($config.firebird)\isql.exe"
 
 # Guarda o diretório do sistema e do banco
-$system_dir = ".\${system}"
+$system_dir = ".\firebird-v${version}\${system}"
 $db_dir = "${system_dir}\bancos\${database}"
 
 # Concatena os caminhos do script de reparação, do banco e do arquivo de log
-$repair_script = "${system_dir}\reparador.sql"
+$repair_scripts = ls -Path "${system_dir}\reparadores\" -File -Filter "*.sql" -Name
+$repair_script = "${system_dir}\reparadores\reparador.sql"
 $db_path = "${db_dir}\in.fdb"
 $log_path = "${db_dir}\reparo.log"
 
